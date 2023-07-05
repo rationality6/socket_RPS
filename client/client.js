6bac9c9df81e56a5d1bc7ff7ad27e569dc2ed651
Hyun Ahn <codepawn@icloud.com>
2023-07-05 13:27:27 +0900

    wip

--- client/client.js
+++ client/client.js
@@ -1,1 +1,50 @@
-console.log("loaded client.js")
+console.log("loaded client.js");
+
+const socket = io();
+let roomId = null;
+
+const createGame = () => {
+  console.log("create");
+  socket.emit("createGame");
+};
+
+const joinGame = () => {
+  console.log("join");
+  roomId = document.getElementById("roomId").value;
+  socket.emit("joinGame", { roomId: roomId });
+};
+
+const createCopyButton = () => {
+  const copyButton = document.createElement("button");
+  copyButton.style.display = "block";
+  copyButton.innerHTML = "Copy code";
+  copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(roomId).then(
+      () => {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  });
+  return copyButton;
+};
+
+socket.on("newGame", (data) => {
+  roomId = data.roomId;
+  document.getElementById("initial").style.display = "none";
+  document.getElementById("gamePlay").style.display = "block";
+
+  document.getElementById(
+    "waitingArea"
+  ).innerHTML = `waiting ${roomId} to join`;
+
+  let copyButton = createCopyButton();
+  document.getElementById("waitingArea").appendChild(copyButton);
+});
+
+socket.on("playersConnected", () => {
+  console.log("playersConnected");
+  document.getElementById("waitingArea").innerHTML = `Players connected`;
+});
--- index.js
+++ index.js
@@ -17,9 +17,43 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
 
-io.on("connection", (socket) => {
+let clientCount = 0;
+let rooms = {};
 
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  clientCount += 1;
+  console.log("clientCount: ", clientCount);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    clientCount -= 1;
+    console.log("clientCount: ", clientCount);
+  });
+
+  socket.on("createGame", () => {
+    console.log("createGame, server");
+
+    const roomId = Math.floor(Math.random() * 100);
+
+    rooms[roomId] = {};
+    socket.join(roomId);
+    socket.emit("newGame", { roomId: roomId });
+  });
+
+  socket.on("joinGame", (data) => {
+    console.log("joinGame, server");
+    console.log("data: ", data.roomId);
+    console.log(rooms);
+    if (rooms[data.roomId] != null) {
+      console.log("exists");
+      socket.join(data.roomId);
+      socket.to(data.roomId).emit("playersConnected", {});
+      socket.emit("playersConnected");
+
+    }
+  });
+});
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
