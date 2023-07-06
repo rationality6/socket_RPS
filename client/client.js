825d40c19bd42b993a12cb6b68c5b49af162481d
Hyun Ahn <codepawn@icloud.com>
2023-07-06 21:16:43 +0900

    roomlist done

--- client/client.js
+++ client/client.js
@@ -1,5 +1,6 @@
 const socket = io();
-let roomId = null;
+let roomId;
+let socketId;
 
 const createGame = () => {
   socket.emit("createGame");
@@ -27,31 +28,79 @@ const createCopyButton = () => {
   return copyButton;
 };
 
+const createJoinRoomDiv = (rooms, roomKey) => {
+  const joinRoomDiv = document.createElement("div");
+  joinRoomDiv.classList = "mouse-over-select";
+  joinRoomDiv.style.display = "block";
+
+  if (roomId == roomKey) {
+    joinRoomDiv.innerHTML = `${roomKey} <span class="border rounded bg-red-300">mine</span>`;
+  } else if (rooms[roomKey].guest != null) {
+    joinRoomDiv.innerHTML = `${roomKey} <span class="border rounded bg-red-300">full</span>`;
+  } else {
+    joinRoomDiv.innerHTML = roomKey;
+    joinRoomDiv.addEventListener("click", () => {
+      socket.emit("joinGame", { roomId: roomKey });
+    });
+  }
+
+  return joinRoomDiv;
+};
+
 socket.on("newGame", (data) => {
-  roomId = data.roomId;
   document.getElementById("initial").style.display = "none";
-  document.getElementById("gamePlay").style.display = "block";
+  document.getElementById("waitingArea").style.display = "block";
 
   document.getElementById(
     "waitingArea"
   ).innerHTML = `waiting ${roomId} to join`;
-
-  let copyButton = createCopyButton();
-  document.getElementById("waitingArea").appendChild(copyButton);
 });
 
 socket.on("playersConnectedToGame", () => {
   document.getElementById("initial").style.display = "none";
   document.getElementById("waitingArea").style.display = "none";
-  document.getElementById("gameArea").style.display = "block";
+  document.getElementById("gamePlay").style.display = "block";
+});
+
+socket.on("setSocketId", (data) => {
+  socketId = data.userId;
+});
+
+socket.on("setRoomId", (data) => {
+  roomId = data.roomId;
 });
 
 socket.on("connectionsChanged", (data) => {
   document.getElementById("clientListArea").innerHTML = "";
-  const idDivs = data.userIds.forEach((id) => {
-    document.getElementById("clientListArea").innerHTML += `<div class="mouse-over-select">${id}</div>`;
+  data.userIds.forEach((id) => {
+    if (id == socketId) {
+      document.getElementById(
+        "clientListArea"
+      ).innerHTML += `<div class="mouse-over-select">
+      ${id}
+      <span class="border rounded bg-red-300">me</span>
+      </div>`;
+    } else {
+      document.getElementById(
+        "clientListArea"
+      ).innerHTML += `<div class="mouse-over-select">${id}</div>`;
+    }
   });
   document.getElementById("clientTotalArea").innerHTML = data.userIds.length;
+});
+
+socket.on("updateRoomList", (data) => {
+  document.getElementById("roomListArea").innerHTML = "";
+
+  Object.keys(data.roomList).forEach((roomKey) => {
+    document
+      .getElementById("roomListArea")
+      .append(createJoinRoomDiv(data.roomList, roomKey));
+  });
+
+  document.getElementById("roomTotalCountArea").innerHTML = Object.keys(
+    data.roomList
+  ).length;
 });
 
 const sendChoice = (choice) => {
--- index.js
+++ index.js
@@ -18,37 +18,53 @@ app.get("/", (req, res) => {
 });
 
 let clientList = [];
-let rooms = {};
+let roomList = {};
 
 io.on("connection", (socket) => {
   clientList.push(socket.id);
 
+  socket.emit("setSocketId", { userId: socket.id });
   io.emit("connectionsChanged", { userIds: clientList });
+  io.emit("updateRoomList", { roomList: roomList });
 
   socket.on("disconnect", () => {
     const targetIndex = clientList.indexOf(socket.id);
+    
+    Object.keys(roomList).forEach((room) => {
+      if(roomList[room].owner == socket.id){
+        delete roomList[room]
+      }
+    })
+
+    socket.leaveAll();
     clientList.splice(targetIndex, 1);
 
     io.emit("connectionsChanged", { userIds: clientList });
+    io.emit("updateRoomList", { roomList: roomList });
   });
 
   socket.on("createGame", () => {
-    console.log("createGame");
+    const roomId = String(Math.floor(Math.random() * 100000));
 
-    const roomId = String(Math.floor(Math.random() * 100));
-
-    rooms[roomId] = {};
+    roomList[roomId] = { owner: socket.id, guest: null };
+    console.log(roomList)
     socket.join(roomId);
+    socket.emit("setRoomId", { roomId: roomId });
     socket.emit("newGame", { roomId: roomId });
+    io.emit("updateRoomList", { roomList: roomList });
   });
 
   socket.on("joinGame", (data) => {
     console.log(`joinGame ${data.roomId}`);
 
-    if (rooms[data.roomId] != null) {
+    if (roomList[data.roomId] != null) {
       socket.join(data.roomId);
+      roomList[data.roomId].guest = socket.id;
       io.to(data.roomId).emit("playersConnectedToGame");
+      io.emit("updateRoomList", { roomList: roomList });
     }
+
+    console.log(roomList)
   });
 });
 
